e3a8f1055d05f40af731247746adee245d4ef78e
Adil <ascodesofficial@gmail.com>
2021-11-13 19:58:59 +0500

    Small Program which checks whether the Alphabet is uppercase or Lowercase

---
+++ Main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    char alpha;
+    printf("Enter the Alphabet = ");
+    scanf("%c", &alpha);
+    if(alpha>=65 && alpha<=90){
+        printf("Your entered alphabet is Uppercase");
+    }else if(alpha>=97 && alpha<=122){
+        printf("Your entered alphabet is Lowercase");
+    }else{
+        printf("You have entered wrong value");
+    }
+    return 0;
+}
